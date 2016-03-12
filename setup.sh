4bbca10f6cc413c31ef994c60cada961c1f040f9
Aaron Jensen <aaronjensen@gmail.com>
2016-03-11 22:17:16 -0800

    Disable app nap

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@
 
 [[ -d ~/.spacemacs.d/layers/+justbur ]] || \
     git clone git@github.com:justbur/spacemacs-layers.git ~/.spacemacs.d/layers/+justbur
+
+# Disable app nap for emacs so it doesn't cause terrible difficult to track down
+# things to happen https://github.com/syl20bnr/spacemacs/issues/5413
+defaults write org.gnu.Emacs NSAppSleepDisabled -bool YES
