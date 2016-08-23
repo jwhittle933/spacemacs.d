b68f2bc6f4cf5585b71e9a642a1ef7d6f0ee8278
Aaron Jensen <aaronjensen@gmail.com>
2016-08-23 08:51:29 -0700

    Run flow first

--- lisp/init-flow.el
+++ lisp/init-flow.el
@@ -1,9 +1,12 @@
 (use-package flycheck-flow
   :config
   (progn
+    ;; Run flow first
+    (setq flycheck-checkers (remove 'javascript-flow flycheck-checkers))
+    (add-to-list 'flycheck-checkers 'javascript-flow)
     (flycheck-add-mode 'javascript-flow 'web-mode)
     (flycheck-add-mode 'javascript-flow 'react-mode)
-    (flycheck-add-next-checker 'javascript-eslint '(t . javascript-flow))))
+    (flycheck-add-next-checker 'javascript-flow '(t . javascript-eslint))))
 
 (require 'flow-types)
 
