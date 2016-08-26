cc482ef63b7dd4ad83117e816b7dc763a9c8a89e
Aaron Jensen <aaronjensen@gmail.com>
2016-08-25 20:23:36 -0700

    Don't bother with flow checking in web-mode

--- lisp/init-flow.el
+++ lisp/init-flow.el
@@ -4,7 +4,6 @@
     ;; Run flow first
     (setq flycheck-checkers (remove 'javascript-flow flycheck-checkers))
     (add-to-list 'flycheck-checkers 'javascript-flow)
-    (flycheck-add-mode 'javascript-flow 'web-mode)
     (flycheck-add-mode 'javascript-flow 'react-mode)
     (flycheck-add-next-checker 'javascript-flow '(t . javascript-eslint))))
 
