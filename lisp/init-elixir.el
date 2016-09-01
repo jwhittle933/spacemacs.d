d766726d615bd407f4a9f9d645d1499736abdce6
Aaron Jensen <aaronjensen@gmail.com>
2016-09-01 07:06:54 -0700

    Use new working-directory in elixir checkers

--- lisp/init-elixir.el
+++ lisp/init-elixir.el
@@ -7,53 +7,31 @@
 (setq alchemist-goto-elixir-source-dir "~/Source/elixir")
 (setq alchemist-goto-erlang-source-dir "~/Source/otp")
 
-(defun elixir-flycheck-project-root ()
+(defun elixir-flycheck-project-root (&rest _ignored)
   (locate-dominating-file buffer-file-name "mix.exs"))
 
-(defun elixir-flycheck-cd-option ()
-  (format "IEx.Helpers.cd(\"%s\")"
-          (elixir-flycheck-project-root)))
-
-;; Add flychecker for dialyzer
 (with-eval-after-load 'flycheck
   (flycheck-define-checker elixir-dogma
     "Defines a checker for elixir with dogma"
-    :command ("elixir" "-e" (eval (elixir-flycheck-cd-option)) "-S"
-              "mix" "dogma" "--format" "flycheck" "--stdin" source-original)
+    :command ("mix" "dogma" "--format" "flycheck" source)
     :predicate
     (lambda ()
-      (let ((root (elixir-flycheck-project-root)))
-        (and
-         root
-         (flycheck-buffer-saved-p)
-         (file-exists-p (concat root "mix.exs"))
-         (file-exists-p (concat root "deps/dogma")))))
-    :standard-input t
+      (file-exists-p "deps/dogma"))
     :error-patterns
     (
      (info line-start (file-name) ":" line ":" column ": " (or "C" "R" "D") ": " (optional (id (one-or-more (not (any ":")))) ": ") (message) line-end)
      (warning line-start (file-name) ":" line ":" column ": " (or "W" "E" "F") ": " (optional (id (one-or-more (not (any ":")))) ": ") (message) line-end))
-    :error-filter
-    (lambda (errors)
-      (dolist (err (flycheck-sanitize-errors errors))
-        (setf (flycheck-error-filename err)
-              (concat (elixir-flycheck-project-root)
-                      (flycheck-error-filename err))))
-      errors)
-    :modes (elixir-mode))
+    :working-directory elixir-flycheck-project-root
+    :modes elixir-mode)
 
   (flycheck-define-checker elixir-dialyzer
     "Erlang syntax checker based on dialyzer."
-    :command ("elixir" "-e" (eval (elixir-flycheck-cd-option)) "-S"
-              "mix" "dialyzer")
+    :command ("mix" "dialyzer")
     :predicate
     (lambda ()
-      (let ((root (elixir-flycheck-project-root)))
-        (and
-         root
-         (flycheck-buffer-saved-p)
-         (file-exists-p (concat root "mix.exs"))
-         (file-exists-p (concat root "deps/dialyxir")))))
+      (and
+       (flycheck-buffer-saved-p)
+       (file-exists-p "deps/dialyxir")))
     :error-patterns
     ((warning line-start
               (file-name)
@@ -69,19 +47,13 @@
             ":"
             (message)
             line-end))
-    :error-filter
-    (lambda (errors)
-      (dolist (err (flycheck-sanitize-errors errors))
-        (setf (flycheck-error-filename err)
-              (concat (elixir-flycheck-project-root)
-                      (flycheck-error-filename err))))
-      errors)
+    :working-directory elixir-flycheck-project-root
     :modes elixir-mode)
 
   (add-to-list 'flycheck-checkers 'elixir-dogma)
   (add-to-list 'flycheck-checkers 'elixir-dialyzer t)
-  (flycheck-add-next-checker 'elixir-credo '(t . elixir-dogma))
-  (flycheck-add-next-checker 'elixir-dogma '(t . elixir-dialyzer)))
+  (flycheck-add-next-checker 'elixir-dogma 'elixir-credo)
+  (flycheck-add-next-checker 'elixir-credo 'elixir-dialyzer))
 
 ;; TODO: Remove after https://github.com/syl20bnr/spacemacs/pull/6983 merged
 (add-hook 'elixir-mode-hook (lambda ()
