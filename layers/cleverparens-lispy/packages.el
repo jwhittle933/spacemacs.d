2828c7e508d61f86f2142c999698b5d184e94352
Aaron Jensen <aaronjensen@gmail.com>
2016-09-15 22:48:31 -0700

    Disable lispy for now

--- layers/cleverparens-lispy/packages.el
+++ layers/cleverparens-lispy/packages.el
@@ -12,13 +12,13 @@
     :config
     (require 'evil-cleverparens-text-objects)))
 
-(defun cleverparens-lispy/init-lispy ()
-  (use-package lispy
-    :defer t
-    :init
-    (add-hook 'emacs-lisp-mode-hook (lambda ()
-                                      (lispy-mode 1)
-                                      (lispy-set-key-theme '(special evilcp c-digits))))))
+;; (defun cleverparens-lispy/init-lispy ()
+;;   (use-package lispy
+;;     :defer t
+;;     :init
+;;     (add-hook 'emacs-lisp-mode-hook (lambda ()
+;;                                       (lispy-mode 1)
+;;                                       (lispy-set-key-theme '(special evilcp c-digits))))))
 
 (defun cleverparens-lispy/pre-init-smartparens ()
   (setq sp-highlight-pair-overlay nil)
