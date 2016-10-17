fb992315c540d3755e4179b65966840b99ff91ed
Aaron Jensen <aaronjensen@gmail.com>
2016-10-17 07:47:05 -0700

    Try moving away from shackle for magit
    It introduced something that broke shackle, so let's try it their way
    for a bit.

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -3,6 +3,7 @@
 (setq magit-auto-revert-mode t)
 (setq magit-revert-buffers 1)
 (setq magit-commit-show-diff nil)
+(setq magit-display-buffer-function 'magit-display-buffer-fullcolumn-most-v1)
 
 ;; Start commit in insert mode
 (add-hook 'git-commit-mode-hook 'evil-insert-state)
@@ -10,10 +11,6 @@
 ;; Use C-n/C-p to navigate sections
 (with-eval-after-load 'magit
   (evil-define-key 'normal magit-mode-map (kbd "C-n") 'magit-section-forward-sibling)
-  (evil-define-key 'normal magit-mode-map (kbd "C-p") 'magit-section-backward-sibling)
-
-  ;; Shackle magit status to right hand side
-  (with-eval-after-load 'shackle
-    (push '(magit-status-mode :align right :size 90) shackle-rules)))
+  (evil-define-key 'normal magit-mode-map (kbd "C-p") 'magit-section-backward-sibling))
 
 (provide 'init-magit)
