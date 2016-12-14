d3c0a713f837a0aeb71420f751b698e7c67bdef5
Aaron Jensen <aaronjensen@gmail.com>
2016-12-13 16:26:04 -0800

    Fix persp-mode annoyance

--- init.el
+++ init.el
@@ -443,6 +443,11 @@ layers configuration. You are free to put any user code."
    dtrt-indent-mode t
    )
 
+  ;; Prevent perp-mode from warning you if you delete a buffer belonging to
+  ;; another perspective. This fixes issues I had w/ projectile capture:
+  ;; https://github.com/syl20bnr/spacemacs/issues/7931#issuecomment-266706480
+  (setq persp-kill-foreign-buffer-action nil)
+
   ;; ivy
   ;; Use fuzzy finder
   (setq ivy-re-builders-alist
