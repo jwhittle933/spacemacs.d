053818668dbfa79ce0a09591a81544f740dfc9f6
Aaron Jensen <aaronjensen@gmail.com>
2017-03-05 22:04:58 -0800

    Scale entire frame font w/ s--/s-=

--- init.el
+++ init.el
@@ -506,6 +506,29 @@ layers configuration. You are free to put any user code."
       (global-unset-key key)
       (global-set-key key (intern (format "eyebrowse-switch-to-window-config-%d" n)))))
 
+  ;; Change entire frame font size
+  (defun my-alter-frame-font-size (fn)
+    (let* ((current-font-name (frame-parameter nil 'font))
+           (decomposed-font-name (x-decompose-font-name current-font-name))
+           (font-size (string-to-int (aref decomposed-font-name 5))))
+      (aset decomposed-font-name 5 (int-to-string (funcall fn font-size)))
+      (set-frame-font (x-compose-font-name decomposed-font-name))))
+
+  (defun my-inc-frame-font-size ()
+    (interactive)
+    (my-alter-frame-font-size '1+))
+
+  (defun my-dec-frame-font-size ()
+    (interactive)
+    (my-alter-frame-font-size '1-))
+
+  (global-set-key (kbd "s-+") 'my-inc-frame-font-size)
+  (global-set-key (kbd "s-=") 'my-inc-frame-font-size)
+  (global-set-key (kbd "s--") 'my-dec-frame-font-size)
+  (global-set-key (kbd "C-+") 'spacemacs/scale-up-font)
+  (global-set-key (kbd "C-=") 'spacemacs/scale-up-font)
+  (global-set-key (kbd "C--") 'spacemacs/scale-down-font)
+
   ;; Pairing stuff
   (global-set-key (kbd "<end>") 'evil-end-of-line)
 
