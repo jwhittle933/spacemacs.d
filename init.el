167bc61b57c30d4c55fff611021c9df3d3f7fdc3
Aaron Jensen <aaronjensen@gmail.com>
2017-04-10 09:25:13 -0700

    Remove magithub, it's too slow for now

--- init.el
+++ init.el
@@ -112,6 +112,7 @@ values."
    ;; A list of packages that will not be installed and loaded.
    dotspacemacs-excluded-packages
    '(
+     magithub
      )
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
