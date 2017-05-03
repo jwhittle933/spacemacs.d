3a4144df33b47b58b775a836ec2975d38f70105a
Aaron Jensen <aaronjensen@gmail.com>
2017-05-03 07:55:28 -0700

    Change fonts

--- init.el
+++ init.el
@@ -193,9 +193,9 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Inconsolatag"
+   dotspacemacs-default-font '("SF Mono"
                                :size 14
-                               :weight normal
+                               :weight light
                                :width normal
                                :powerline-scale 1.1)
    ;; The leader key
