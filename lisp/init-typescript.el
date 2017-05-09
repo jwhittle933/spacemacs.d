713f8664fca3237cb04678ca80246d9f76927819
Aaron Jensen <aaronjensen@gmail.com>
2017-05-09 14:40:51 -0700

    Remove tide workaround, it was merged and released

--- lisp/init-typescript.el
+++ lisp/init-typescript.el
@@ -1,12 +1,6 @@
 (setq typescript-indent-level 2)
 
-;; Work around https://github.com/ananthakumaran/tide/issues/144
-;; Remove after https://github.com/ananthakumaran/tide/pull/146 is merged
-(defun aj/fix-tide-start-server (start-server &rest args)
-  (let ((process-connection-type nil))
-    (apply start-server args)))
 (with-eval-after-load 'tide
-  (advice-add 'tide-start-server :around 'aj/fix-tide-start-server)
   (add-hook 'before-save-hook #'tide-format-before-save))
 
 (provide 'init-typescript)
