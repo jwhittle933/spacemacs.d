c59cf6065bf540bf65f566ad8c4ec97962f560ad
Aaron Jensen <aaronjensen@gmail.com>
2017-05-09 07:43:08 -0700

    Work around tide issue
    https://github.com/ananthakumaran/tide/pull/146

--- lisp/init-typescript.el
+++ lisp/init-typescript.el
@@ -1,3 +1,11 @@
 (setq typescript-indent-level 2)
 
+;; Work around https://github.com/ananthakumaran/tide/issues/144
+;; Remove after https://github.com/ananthakumaran/tide/pull/146 is merged
+(defun aj/fix-tide-start-server (start-server &rest args)
+  (let ((process-connection-type nil))
+    (apply start-server args)))
+(with-eval-after-load 'tide
+  (advice-add 'tide-start-server :around 'aj/fix-tide-start-server))
+
 (provide 'init-typescript)
