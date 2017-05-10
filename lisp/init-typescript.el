833c684775275b84bf66c5a15f2433a0fa84ea51
Aaron Jensen <aaronjensen@gmail.com>
2017-05-09 20:57:07 -0700

    Fix ,f

--- lisp/init-typescript.el
+++ lisp/init-typescript.el
@@ -1,8 +1,7 @@
 (setq typescript-indent-level 2)
 
 (with-eval-after-load 'tide
-  (add-hook 'before-save-hook #'tide-format-before-save))
-
-(spacemacs/set-leader-keys-for-minor-mode 'tide-mode "f" #'tide-fix)
+  (add-hook 'before-save-hook #'tide-format-before-save)
+  (spacemacs/set-leader-keys-for-minor-mode 'tide-mode "f" #'tide-fix))
 
 (provide 'init-typescript)
