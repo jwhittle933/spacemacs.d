9b261b788bc6628a61aab530144818b4a4c4e8fa
Aaron Jensen <aaronjensen@gmail.com>
2017-05-09 20:46:37 -0700

    Add ,f to tide-fix

--- lisp/init-typescript.el
+++ lisp/init-typescript.el
@@ -3,4 +3,6 @@
 (with-eval-after-load 'tide
   (add-hook 'before-save-hook #'tide-format-before-save))
 
+(spacemacs/set-leader-keys-for-minor-mode 'tide-mode "f" #'tide-fix)
+
 (provide 'init-typescript)
